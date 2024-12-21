630b74dbf10ca9f49cc2fa49362906e10ec6d62f
Felipe Rodrigues <felipe.git@outlook.com>
2024-12-21 00:37:24 -0300

    Excluindo o allowed hosts2

--- app/settings.py
+++ app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-647z8@+t6w#o@5b)+vo@(jw=y=z1nk7!@0p+$a5^yso+&#pvem
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
