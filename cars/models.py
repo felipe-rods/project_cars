a828b69e3cb981246cbb9aac66eca2c129bffbac
Felipe Rodrigues <felipe.git@outlook.com>
2025-02-16 21:53:27 -0300

    Fixing CarInventory bug

--- cars/models.py
+++ cars/models.py
@@ -26,7 +26,7 @@ class Car(models.Model):
 
 class CarInventory(models.Model):
     cars_count = models.IntegerField()
-    cars_price = models.DecimalField()
+    cars_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
